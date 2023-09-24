3992a4a84d8f137328c65341ee99ea3152d4b095
BuildTools <unconfigured@null.spigotmc.org>
2023-09-24 14:53:50 +0530

    adding all the stuff

---
+++ abrir.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while true; do
+	java -Xms10G -Xmx10G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1MaxNewSizePercent=60 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar paper-custompots-367.jar
+	echo "Restarting in 5 seconds. Press CTRL + C to cancel."
+	sleep 5s
+done
